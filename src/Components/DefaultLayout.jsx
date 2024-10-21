bdf90465e3ddb3c5d89de2fbc0f5c351c890e68f
Arpit Tiwari <tdm75513@gmail.com>
2024-10-21 11:38:17 +0530

    .

--- src/Components/DefaultLayout.jsx
+++ src/Components/DefaultLayout.jsx
@@ -3,6 +3,7 @@ import { useSwipeable } from 'react-swipeable';
 import DefaultBackground from '../../assets/images/default-background.png';
 import HamburgerMenu from '../../assets/icons/hamburger-menu.svg';
 import Sidebar from '../Components/Sidebar';
+import HeroSection from './HeroSection';
 
 function DefaultLayout({ children }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -21,7 +22,7 @@ function DefaultLayout({ children }) {
     <div className="min-h-screen bg-black flex justify-center">
       <div 
         {...handlers}
-        className="w-[425px] min-h-screen bg-cover bg-center relative overflow-hidden"
+        className="w-[400px] min-h-screen bg-cover bg-center relative overflow-hidden"
         style={{ backgroundImage: `url(${DefaultBackground})` }}
       >
         <header className="absolute top-3 left-0 w-full z-10">
@@ -51,9 +52,11 @@ function DefaultLayout({ children }) {
 
         <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
 
-        <div className={`relative z-10 ${isSidebarOpen ? 'opacity-50' : ''}`}>
+        {/* <div className={`relative z-10 ${isSidebarOpen ? 'opacity-50' : ''}`}>
           {children}
-        </div>
+        </div> */}
+
+        <HeroSection />
 
         {isSidebarOpen && (
           <div className="absolute inset-0 bg-black opacity-50 z-20" onClick={() => setIsSidebarOpen(false)} />
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import HeroImage from '../../public/assets/HeroImage.svg'
+import LogoInHeroSection from '../../public/assets/LogoInHeroSection.svg'
 
 function HeroSection() {
   return (
-    <div>
+    <div className='mt- h-[640px] bg-[#002D3A] border border-black rounded-b-[50px]'>
+
+      {/* jdhfkjdsh */}
+      <div className="h-[400px] relative rounded-b-[50px] pt-20  bg-[#FFB404]">
+        <img className='w-80 ml-8' src={LogoInHeroSection} alt="" />
+        <img className='' src={HeroImage} alt="" />
+      </div>
       
     </div>
   )
