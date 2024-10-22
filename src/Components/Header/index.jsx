5797f12ed50e43895e650ff7b0a56bd8702bf99e
ManishSaroj <manishsaroj5259@gmail.com>
2024-10-22 15:14:42 +0530

    .

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -30,16 +30,16 @@ function Header({ toggleSidebar }) {
         </div>
         <div className="flex-grow p-4 h-[50px] flex justify-center items-center bg-[#002D3A] rounded-l-full ml-5 shadow-xl shadow-red-500/45 ">
         <span className="text-white font-bold text-[0.83rem] text-left w-full">Safety is Everyone&apos;s responsibility</span>
-          <div className="w-[70px] h-[25px] bg-[#0F4254] absolute top-[2.2rem] right-0" style={{
+          <div className="w-[70px] h-[25px] bg-[#0F4254] absolute top-[2.9rem] right-0" style={{
             clipPath: 'polygon(0 0, 100% 0, 100% 100%, 20% 100%)'
           }}></div>          
-          <div className="w-[20px] h-[20px] bg-gradient-to-t from-[#FFB404] to-[#FFCE05] absolute top-6 right-[56px]" style={{
+          <div className="w-[20px] h-[20px] bg-gradient-to-t from-[#FFB404] to-[#FFCE05] absolute top-9 right-[56px]" style={{
             clipPath: 'polygon(0 0, 70% 0, 100% 100%, 30% 100%)'
           }}></div>
-           <div className="w-[20px] h-[20px] bg-gradient-to-t from-[#FFB404] to-[#FFCE05] absolute top-6 right-[33px]" style={{
+           <div className="w-[20px] h-[20px] bg-gradient-to-t from-[#FFB404] to-[#FFCE05] absolute top-9 right-[33px]" style={{
             clipPath: 'polygon(0 0, 70% 0, 100% 100%, 30% 100%)'
           }}></div>
-          <div className="w-[30px] h-[20px] bg-gradient-to-t from-[#FFB404] to-[#FFCE05] absolute top-6 right-0" style={{
+          <div className="w-[30px] h-[20px] bg-gradient-to-t from-[#FFB404] to-[#FFCE05] absolute top-9 right-0" style={{
             clipPath: 'polygon(0 0, 100% 0, 100% 100%, 20% 100%)'
           }}></div>
           </div>
