024f5122f7c07dc95792e90c4b7d679fc03f81c1
Arpit Tiwari <tdm75513@gmail.com>
2024-10-22 14:39:24 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
