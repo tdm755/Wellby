097c3871485ff79b0e799d3a90b1d49687dce473
Arpit Tiwari <tdm75513@gmail.com>
2024-10-22 18:08:25 +0530

    .

--- src/Utils/DashboardInputs.jsx
+++ src/Utils/DashboardInputs.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function DashboardInputs({label, type, value, icon, icon2, placeholder}) {
+function DashboardInputs({label, type, value, icon, icon2, placeholder, readOnlyOf}) {
   return (
     <div className='flex flex-col'>
       <label className=' left-10 -top-3 px-4 bg-white text-sm text-[#FF9E00] ' htmlFor="">{label}</label>
-      <input className='outline-none font-semibold text-[19px] text-[#3C3C3C] pl-14 border-[1.5px] w-full rounded-lg bg-transparent h-[51px] border-[#C8C8C8]' placeholder={placeholder} type={type} value={value} />
+      <input className={`outline-none font-semibold text-[19px] text-[#3C3C3C] pl-14 border-[1.5px] w-full rounded-lg bg-transparent h-[51px] border-[#C8C8C8] ${readOnlyOf && 'bg-[F5F5F5]'}`} placeholder={placeholder} type={type} value={value} readOnly={readOnlyOf} />
       <img className='w-6 h-6 absolute top-3 left-3' src={icon} alt="" />
       {icon2 && <img className='cursor-pointer w-6 h-6 absolute top-3 right-3' src={icon2} alt="" />}
     </div>
