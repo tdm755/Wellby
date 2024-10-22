8ef20c0c7498698e955fd2bda6a432ea9011fd98
ManishSaroj <manishsaroj5259@gmail.com>
2024-10-22 15:24:37 +0530

    .

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Logo from '../../../public/logo.svg'
 import HamburgerMenu from '../../../public/assets/icons/hamburger-menu.svg';
+import { useLocation } from 'react-router-dom';
 
 function Header({ toggleSidebar }) {
   const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
+  const isDashboard = location.pathname === '/dashboard';
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,14 +24,22 @@ function Header({ toggleSidebar }) {
     <header className={`fixed w-[400px] py-3 top-0 items-center justify-center z-50 transition-colors duration-300 ${isScrolled ? 'bg-[#FFCE05] shadow-md rounded-bl-[50px]' : ''}`}>
       <div className="flex items-center">
         <div className="p-5">
-          <img 
-            src={HamburgerMenu} 
-            alt="Menu" 
-            className="w-[25px] h-[30px] cursor-pointer" 
-            onClick={toggleSidebar}
-          />
+          {isDashboard ? (
+            <img 
+              src={Logo} 
+              alt="Logo" 
+              className="w-[35px]" 
+            />
+          ) : (
+            <img 
+              src={HamburgerMenu} 
+              alt="Menu" 
+              className="w-[25px] h-[30px] cursor-pointer" 
+              onClick={toggleSidebar}
+            />
+          )}
         </div>
-        <div className="flex-grow p-4 h-[50px] flex justify-center items-center bg-[#002D3A] rounded-l-full ml-5 shadow-xl shadow-red-500/45 ">
+        <div className="flex-grow p-4 h-[50px] flex justify-center items-center bg-[#002D3A] rounded-l-full shadow-xl shadow-red-500/45 ">
         <span className="text-white font-bold text-[0.83rem] text-left w-full">Safety is Everyone&apos;s responsibility</span>
           <div className="w-[70px] h-[25px] bg-[#0F4254] absolute top-[2.9rem] right-0" style={{
             clipPath: 'polygon(0 0, 100% 0, 100% 100%, 20% 100%)'
--- src/Components/Pages/Dashboard.jsx
+++ src/Components/Pages/Dashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../Header'
+import DefaultBackground from '../../../public/assets/images/default-background.png'
 
 function Dashboard() {
   return (
-    <div>
-      <Header />
+    <div className='min-h-screen bg-black flex justify-center'>
+      <div className='w-[400px]' style={{ backgroundImage: `url(${DefaultBackground})` }}>
+        <Header />
+      </div>
     </div>
   )
 }
