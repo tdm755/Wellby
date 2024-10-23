099f439e7b537193d32fdce3cd41fd9f2b56279d
ManishSaroj <manishsaroj5259@gmail.com>
2024-10-23 17:39:03 +0530

    .

--- src/Components/Header/DashboardHeader.jsx
+++ src/Components/Header/DashboardHeader.jsx
@@ -8,14 +8,9 @@ function DashboardHeader() {
   const navigate = useNavigate();
   const [isScrolled, setIsScrolled] = useState(false);
 
-  // Add scroll event listener
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 50);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -26,25 +21,45 @@ function DashboardHeader() {
 
   return (
     <header 
-      className={`fixed w-full max-w-[400px] py-3 top-0 items-center justify-center z-40 transition-colors duration-300 ${isScrolled ? 'bg-[#FFCE05] shadow-md rounded-bl-[50px]' : ''}`}
+      className={`fixed w-full max-w-[400px] py-3 top-0 items-center justify-center z-40 transition-all duration-300 ${
+        isScrolled ? 'bg-[#002D3A] shadow-md rounded-bl-[50px]' : ''
+      }`}
     >
       <div className="flex items-center flex-wrap">
-        <div className="flex items-center justify-center w-[15%]">
-          <img
-            src={Logo}
-            alt="Logo"
-            className="w-8 cursor-pointer"
-            onClick={() => { navigate('/'); }}
-          />
-        </div>
-        <div className="flex-grow w-[85%] h-11 flex justify-center items-end bg-[#002D3A] rounded-l-full shadow-xl shadow-red-500/45">
-          <div className="border-white h-full flex items-center justify-center w-full">
-            <span className="text-white whitespace-nowrap font-bold text-center w-full text-[0.80rem]">
+        {!isScrolled ? (
+          <>
+            <div className="flex items-center justify-center w-[15%]">
+              <img
+                src={Logo}
+                alt="Logo"
+                className="w-8 cursor-pointer"
+                onClick={() => { navigate('/'); }}
+              />
+            </div>
+            <div className="flex-grow w-[85%] h-11 flex justify-center items-end bg-[#002D3A] rounded-l-full shadow-xl shadow-red-500/45">
+              <div className="border-white h-full flex items-center justify-center w-full">
+                <span className="text-white whitespace-nowrap font-bold text-center w-full text-[0.80rem]">
+                  Safety is Everyone&apos;s responsibility
+                </span>
+              </div>
+              <img className='h-8' src={HeaderRightSideLogo} alt="Header Right Logo" />
+            </div>
+          </>
+        ) : (
+          <div className="flex items-center justify-between w-full px-4 relative">
+            <span className="text-white whitespace-nowrap font-bold text-[1rem] absolute left-1/2 transform -translate-x-1/2 pr-6">
               Safety is Everyone&apos;s responsibility
             </span>
+            <div className="ml-auto">
+              <img
+                src={LogoutIcon}
+                alt="Logout"
+                className="w-8 h-8 cursor-pointer"
+                onClick={() => {/* Add logout functionality */}}
+              />
+            </div>
           </div>
-          <img className='h-8' src={HeaderRightSideLogo} alt="Header Right Logo" />
-        </div>
+        )}
       </div>
     </header>
   );
